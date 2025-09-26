b5242fcca2609d3d2e9a1b65c0e6b9b1b928bbac
XfheDev <88056186+XfheDev@users.noreply.github.com>
2025-09-26 19:49:46 +0300

    Time: 0 ms (100.00%) | Memory: 8.4 MB (97.24%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -5,13 +5,14 @@ public:
         const int INT_MAX_DIV10 = INT_MAX / 10;
         const int INT_MIN_DIV10 = INT_MIN / 10;
 
-        while (x != 0) {
+        while (x) {
             int pop = x % 10;
             x /= 10;
 
-            // Overflow kontrolü branch minimal
-            if (rev > INT_MAX_DIV10 || (rev == INT_MAX_DIV10 && pop > 7)) return 0;
-            if (rev < INT_MIN_DIV10 || (rev == INT_MIN_DIV10 && pop < -8)) return 0;
+            // Branchless-ish overflow check
+            if (rev > INT_MAX_DIV10 || (rev == INT_MAX_DIV10 && pop > 7) ||
+                rev < INT_MIN_DIV10 || (rev == INT_MIN_DIV10 && pop < -8))
+                return 0;
 
             rev = rev * 10 + pop;
         }
